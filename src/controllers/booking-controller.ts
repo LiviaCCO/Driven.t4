1903c0d01574dafe1f1cea057c960c51bb055ed8
LiviaCCO <livia.ccapistrano@gmail.com>
2023-07-26 23:26:45 -0300

    primeiros passos

---
+++ src/controllers/booking-controller.ts
@@ -0,0 +1,16 @@
+import httpStatus from 'http-status';
+import { Response } from 'express';
+import { AuthenticatedRequest } from '@/middlewares';
+import bookingService from '@/services/booking-service';
+
+export async function getBooking(req: AuthenticatedRequest, res: Response) {
+  
+}
+
+export async function postBooking(req: AuthenticatedRequest, res: Response) {
+  
+}
+
+export async function putBooking(req: AuthenticatedRequest, res: Response) {
+  
+}
